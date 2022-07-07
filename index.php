a213b682676907ec3a4e62e658ae5a9189ad6a0b
br4nc4 <brancaleon.giuseppe@gmail.com>
2022-07-07 17:01:54 +0200

    length paragrafo

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php 
+$parag = "La meccanica quantistica è la teoria fisica che descrive il comportamento della materia, della radiazione e le reciproche interazioni, con particolare riguardo ai fenomeni caratteristici della scala di lunghezza o di energia atomica e subatomica, dove le precedenti teorie classiche risultano inadeguate.";
+
+echo $parag;
+echo "<br>";
+echo "<strong>Lunghezza paragrafo:</strong>" . strlen($parag);
+?>
+
